2e6c5149c7f9a607bf608cc096fc7f4edc19c370
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-02-25 13:08:41 -0300

    list

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,55 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app id="inspire">
+    <v-system-bar app>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row>
+          <template v-for="n in 4">
+            <v-col :key="n" class="mt-2" cols="12">
+              <strong>Category {{ n }}</strong>
+            </v-col>
+
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
+              <!-- <v-sheet height="150"></v-sheet> -->
+              <Card />
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from "vue";
+import Card from "./components/Card.vue";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    Card,
   },
 
-  data: () => ({
-    //
-  }),
+  data: () => ({ drawer: null }),
 });
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-card-title v-for="(item, k) in items" :key="k" :style="item.style">
+      {{ item.title }}
+    </v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+const rcg = () => {
+  const rng = Math.random() * 255;
+  return `hsl(${rng},100%,50%)`;
+};
+
+export default Vue.extend({
+  name: "Card",
+
+  data: () => ({
+    items: [
+      { title: "Yes", style: `color: ${rcg()}` },
+      { title: "sada", style: `color: ${rcg()}` },
+    ],
+  }),
+});
+</script>
